949ee6492e93843f619ad82ecc13a9e8846b124e
songsong <xiongsongsong@outlook.com>
2013-12-15 13:51:14 +0800

    try re-build

--- www/js/index.js
+++ www/js/index.js
@@ -26,11 +26,7 @@ var app = {
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
-        setTimeout(function(){
-            alert('start sjplus')
-            app.receivedEvent('deviceready');
-        },10000)
-        //document.addEventListener('deviceready', this.onDeviceReady, false);
+        document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     // deviceready Event Handler
     //
@@ -41,58 +37,17 @@ var app = {
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        alert(KISSY)
-        var S=KISSY
-        if(id==='deviceready'){
-            var content= S.get('#content')
-        var hostDomain = 'http://www.sjplus.cn'
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
 
-            alert(content)
-        function shuffle(o) { //v1.0
-            for (var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
-            return o;
-        }
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
 
-        KISSY.use('dom,event,io,anim', function (S, DOM, Event, IO, Anim) {
-            alert('kissy use ready')
+        setTimeout(function(){
+            window.location.href='http://www.sjplus.cn/go/front-tv-tianmao'
+        },3000)
 
-            var data = []
-
-            function getData(cb) {
-                IO({
-                    url: hostDomain + '/design-works/latest/list',
-                    dataType: 'jsonp',
-                    success: function (result) {
-                        data = shuffle(result.data)
-                        if (cb) cb()
-                    }
-                })
-            }
-
-            setInterval(getData, 300000)
-
-            var i = 0
-
-            var content = S.get('#content')
-
-            function switchDesignWorks() {
-                DOM.css(content, {
-                    backgroundImage: 'url(' + hostDomain + '/read/' + data[i].file_id.substring(0,24)+ '?m=full-size)'
-                })
-                var nextObj = data[++i]
-                if (!nextObj) {
-                    i = 0
-                    nextObj = data[i]
-                }
-                new Image().src = hostDomain + '/read/' + nextObj.file_id.substring(0,24)+'?m=full-size'
-                setTimeout(switchDesignWorks, 5000)
-            }
-
-            getData(function () {
-                switchDesignWorks()
-            })
-
-        })
-    }
+        console.log('Received Event: ' + id);
     }
 };
