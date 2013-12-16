7bc3bd7640a822d5aace0d21536907d799b2bfc5
songsong <xiongsongsong@outlook.com>
2013-12-16 09:29:53 +0800

    try re-build

--- www/js/index.js
+++ www/js/index.js
@@ -40,67 +40,8 @@ var app = {
     receivedEvent: function (id) {
 
         if (id !== 'deviceready') return
-        var hostDomain = 'http://www.sjplus.cn'
-        var switchCl
 
-        function shuffle(o) { //v1.0
-            for (var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
-            return o;
-        }
-
-        var data = []
-
-        function getData() {
-            clearTimeout(switchCl)
-            $.ajax({
-                url: 'http://www.sjplus.cn/design-works/latest/list',
-                data: {
-                    count: 10
-                },
-                type: 'get',
-                dataType: 'jsonp',
-                success: function (result) {
-                    console.log(result)
-                    data = shuffle(result.data)
-                    switchDesignWorks()
-                }
-            })
-        }
-
-        setInterval(getData, 300000)
-        var i = 0
-        var content = $('#content')
-        var user = $('#user')
-        var title = $('#title')
-        var avatar = $('#avatar')
-
-        function switchDesignWorks() {
-
-            var current = data[i]
-            content.css({
-                backgroundImage: 'url(' + hostDomain + '/read/' + current.file_id.substring(0, 24) + '?m=full-size)'
-            })
-
-            avatar.animate({marginLeft: -180, opacity: 0}, 500, function () {
-                avatar.animate({marginLeft: 0, opacity: 1}, 500)[0].src = 'http://www.sjplus.cn/avatar/' + current.owner_id + '_80x80'
-            })
-            user.animate({paddingLeft: 30, marginBottom: 50, opacity: 0}, 600, function () {
-                user.css({paddingLeft: 0, marginBottom: 0}).animate({ opacity: 1}, 200).html(current.owner_user)
-
-            })
-            title.animate({paddingLeft: 30, opacity: 0}, 600, function () {
-                title.css({paddingLeft: 0}).animate({opacity: 1}, 200).html(current.title)
-            })
-            var nextObj = data[++i]
-            if (!nextObj) {
-                i = 0
-                nextObj = data[i]
-            }
-            new Image().src = hostDomain + '/read/' + nextObj.file_id.substring(0, 24) + '?m=full-size'
-            switchCl = setTimeout(switchDesignWorks, 7000)
-        }
-
-        getData()
+        window.location.href='http://www.sjplus.cn/go/front-tv-tianmao'
 
     }
 };
